277ee47fcc8da399f5b2d5eed425cd25a70531d2
SUMON <sumon01333581557.gmail.com>
2026-05-19 06:48:21 +0600

    Final commit

--- src/proxy.js
+++ src/proxy.js
@@ -2,19 +2,12 @@ import { NextResponse } from 'next/server'
 import { auth } from './lib/auth'
 import { headers } from 'next/headers'
 
-export async function middleware(request) {
+export async function proxy(request) {
     const session = await auth.api.getSession({
         headers: headers()
-    })
+    });
 
-    const { pathname } = request.nextUrl;
-
-    // protect only doctor details + dashboard
-    const isProtected =
-        pathname.startsWith('/doctors/') ||
-        pathname.startsWith('/dashboard');
-
-    if (isProtected && !session) {
+    if (!session) {
         return NextResponse.redirect(new URL('/log-in', request.url));
     }
 
@@ -22,5 +15,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-    matcher: ['/doctors/:path*'],
+    matcher: [ '/dashboard'],
 };
